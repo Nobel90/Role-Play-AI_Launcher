8ff9141004a0b3b962d56854443b5aa6e04fea98
Nobel90 <mostafa_nabil_90@hotmail.com>
2025-10-13 04:07:37 +0300

    feat: Implement electron-updater for self-updating

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // main.js
 
 const { app, BrowserWindow, ipcMain, dialog, shell } = require('electron');
+const { autoUpdater } = require('electron-updater');
 const path = require('path');
 const fs = require('fs').promises; // Use promises-based fs
 const fsSync = require('fs'); // Use sync for specific cases if needed
@@ -39,9 +40,33 @@ function createWindow() {
     });
     win.loadFile('index.html');
     downloadManager = new DownloadManager(win);
+
+    autoUpdater.checkForUpdatesAndNotify();
 }
 
 app.whenReady().then(createWindow);
+
+autoUpdater.on('checking-for-update', () => {
+    console.log('Checking for update...');
+});
+autoUpdater.on('update-available', (info) => {
+    console.log('Update available.');
+});
+autoUpdater.on('update-not-available', (info) => {
+    console.log('Update not available.');
+});
+autoUpdater.on('error', (err) => {
+    console.log('Error in auto-updater. ' + err);
+});
+autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+    console.log(log_message);
+});
+autoUpdater.on('update-downloaded', (info) => {
+    console.log('Update downloaded');
+});
 
 ipcMain.on('open-external', (event, url) => {
     shell.openExternal(url);
